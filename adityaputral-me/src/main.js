cec2deaf73a522999c6cc4f0276cba9e6ce9b0f2
Aditya Putra Leksono <adityaputral.dev@gmail.com>
2020-04-10 16:51:28 +0700

    add analytics

--- adityaputral-me/src/main.js
+++ adityaputral-me/src/main.js
@@ -2,11 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueAnalytics from "vue-analytics";
 
 Vue.config.productionTip = false;
 
 // Importing the global css file
 import "@/assets/css/main.css";
+
+Vue.use(VueAnalytics, {
+  id: "UA-163299388-1",
+  router
+});
 
 new Vue({
   router,
